420161a9bff4460138771fe4bdddec688b0a7d8a
Ilana A. Souza-Concilio <souza.ilana@gmail.com>
2020-04-02 19:45:49 -0300

    Código funcionando sem padrão MVC e com icone correto

--- Jokenpo/ViewController.swift
+++ Jokenpo/ViewController.swift
@@ -12,15 +12,46 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imagemJogador: UIImageView!
     @IBOutlet weak var imagemMaquina: UIImageView!
+    @IBOutlet weak var resultado: UILabel!
+    
+    let opcoes = ["pedra", "papel", "tesoura"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        imagemJogador        // Do any additional setup after loading the view.
+        imagemJogador.image = #imageLiteral(resourceName: "place")
+        imagemMaquina.image = #imageLiteral(resourceName: "place")
+       
+        
+   
     }
 
-    @IBAction func botaoSelecionado(_ sender: Any) {
+    @IBAction func botaoSelecionado(_ sender: UIButton) {
+    
+        let maquina = opcoes.randomElement()!
+        let jogador = sender.currentTitle!
+        imagemMaquina.image = UIImage(named:maquina+".jpg")
+        
+        imagemJogador.image = UIImage(named: jogador+".jpg")
+        
+        verificaResultado(resJogador: jogador, resMaquina: maquina)
     }
+    
+    func verificaResultado(resJogador: String, resMaquina: String){
+        
+        if resJogador == resMaquina{
+            resultado.textColor = UIColor.yellow
+            resultado.text = "Empatou!"
+        }else if resJogador == "pedra" && resMaquina == "tesoura" || resJogador == "papel" && resMaquina == "pedra" || resJogador == "tesoura" && resMaquina == "papel"{
+            resultado.textColor = UIColor.green
+            resultado.text = "Você Ganhou!"
+        }else {
+            resultado.textColor = UIColor.red
+            resultado.text = "Você Perdeu!"
+        }
+        
+    }
+    
     
 }
 
